300d637fa793cb4e6bad3222a44b2991407fcd3b
nassir <nassirmz@gmail.com>
2017-02-02 17:56:57 -0800

    Create a store and give it Provider component

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-//Wrap outer react component with ReactRecux Provider and render to the DOM
+import store from './store/configureStore';
+
+// Wrap outer react component with ReactRecux Provider and render to the DOM
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <div>Domain Lists</div>
   </Provider>,
   document.getElementById('app'),
