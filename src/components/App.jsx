7e2b513ee547e30ccc6a57ca378ab13eaad7e4f6
nassir <nassirmz@gmail.com>
2017-02-02 22:59:21 -0800

    Add outer App component

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// functional component that renders to the page
+const App = () => {
+  return (
+    <div>Domain Lists</div>
+  );
+};
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store/configureStore';
+import App from './components/App';
 
 // Wrap outer react component with ReactRecux Provider and render to the DOM
 ReactDOM.render(
   <Provider store={store}>
-    <div>Domain Lists</div>
+    <App />
   </Provider>,
   document.getElementById('app'),
 );
