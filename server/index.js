06504d47749db40f49f044c30a7c58197f38fe13
nassir <nassirmz@gmail.com>
2017-02-02 17:23:17 -0800

    Add airbnb Eslint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const path = require('path');
 
+//get express app
+const app = express();
+const port = process.env.PORT || 4568;
+
 // middlewares
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.listen(port, function () {
+  console.log('Listenening on port', port);
+})
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-
+//Wrap outer react component with ReactRecux Provider and render to the DOM
 ReactDOM.render(
   <Provider>
     <div>Domain Lists</div>
