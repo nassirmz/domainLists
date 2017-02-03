7aa6d7f508d7162eae1880202519f003b67c25f9
nassir <nassirmz@gmail.com>
2017-02-03 10:42:15 -0800

    Wrote some more tests

--- test/src/actionSpecs.js
+++ test/src/actionSpecs.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { getLists } from '../../src/actions';
+import { getLists, getListDetail } from '../../src/actions';
 
 describe('Actions', () => {
   describe('getLists', () => {
@@ -10,6 +10,15 @@ describe('Actions', () => {
     it('Should return a list of domains', () => {
       const array = ['lists1', 'list2'];
       expect(getLists(array).lists).to.equal(array);
+    });
+  });
+  describe('getListDetail', () => {
+    it('should return an object with the proper type', () => {
+      expect(getListDetail().type).to.equal('GET_LIST_DETAIL');
+    });
+    it('Should return a list detail', () => {
+      const obj = { domains: 'nassir.lol' };
+      expect(getListDetail(obj).listDetail).to.equal(obj);
     });
   });
 });
