6cda085275e5ad0bd6d2c6eb8212d4ddc4ae1357
nassir <nassirmz@gmail.com>
2017-02-03 01:25:32 -0800

    Use CheckMark font icon

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,7 +4,7 @@ const ListItem = ({ domain, price }) => {
   return (
     <tr>
       <td>{domain}</td>
-      <td>{/\.com$|\.lol$/.test(domain) ? 'checkMark' : ''}</td>
+      <td>{/\.com$|\.lol$/.test(domain) ? (<i className="fa fa-check" aria-hidden="true" />) : ''}</td>
       <td>{price}</td>
     </tr>
   );
