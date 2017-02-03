0270ea3d85f8314c1175ed96b18ce9a8dc9e0f13
nassir <nassirmz@gmail.com>
2017-02-02 16:23:53 -0800

    Add Outer React component

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+
+ReactDOM.render(
+  <Provider>
+    <div>Domain Lists</div>
+  </Provider>,
+  document.getElementById('app'),
+);
