3d67d92a83a860a98d02989c142c1ff87a78e60a
nassir <nassirmz@gmail.com>
2017-02-02 16:24:25 -0800

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: __dirname,
+    filename: '/public/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1'],
+        },
+        test: /\.jsx$/,
+        exclude: [/node_modules/],
+      }
+    ]
+  }
+}
